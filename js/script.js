585d53fc09a8354b599e7110f2907714e0544fae
NabilMustofaa <nabilmustofa6@gmail.com>
2022-01-22 14:41:04 +0700

    menambahkan javascript untuk nav dan ubah font

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const header = document.querySelector("nav");
+const sectionOne = document.querySelector(".main");
+
+const sectionOneOptions = {
+  rootMargin: "-100px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
